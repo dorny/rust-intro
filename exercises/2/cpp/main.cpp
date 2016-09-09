bd9d6d4958f71ecc2debfc05d46198f46e244b48
Michal Dorner <dorner.michal@gmail.com>
2016-09-09 10:34:44 +0200

    Improve second c++ anti-example

--- exercises/2/cpp/main.cpp
+++ exercises/2/cpp/main.cpp
@@ -1,19 +1,44 @@
+// This is an c++ anti-example
+// Sometimes it will ends normally, sometimes it will crash.
+// Your task is to find out WHY.
+// Good luck ;)
+
 #include<iostream>
 #include<queue>
 #include<unordered_map>
 #include<functional>
 #include<memory>
 #include<thread>
+#include<mutex>
 
+// Server response to RPC.
+// For purpose of this example, we will just use an empty struct.
+struct Response{};
+
+// Client representation of send RPC.
 struct Request {
-    std::function<void()> callback;
+
+    // Callback will be invoked when server returns response.
+    std::function<void(Response)> callback;
 };
+
 
 class Client {
 private:
+    // Channel to communicate with server.
     std::shared_ptr<std::queue<int>> _channel;
+
+    // Request ID counter. We will just increment it every time, so we have always new unique ID.
     int _requestId = 0;
-    std::unordered_map<int,Request> _pendingRequest;
+
+    // Mutex guarding access to pending requests
+    std::mutex _mut;
+
+    // Storage for pending requests.
+    // Yes we support multiple request done in parallel.
+    std::unordered_map<int,Request> _pendingRequests;
+
+    // Flag that the client was closed.
     bool _closed = false;
 
 public:
@@ -22,32 +47,34 @@ public:
         : _channel(channel)
     { }
 
-    bool invoke(int operationId, std::function<void()> onResponse)
-    {
+    bool invoke(int operationId, std::function<void(Response)> onResponse) {
+        std::lock_guard<std::mutex> lock(_mut);
+
         if (_closed) {
             return false;
         }
 
         int id = ++_requestId;
-        _pendingRequest[id] = Request { onResponse };
+        _pendingRequests[id] = Request { onResponse };
         _channel->push(id);
 
         return true;
     }
 
     void processResponse(int requestId) {
-        auto it = _pendingRequest.find(requestId);
+        std::lock_guard<std::mutex> lock(_mut);
 
-        if (it != _pendingRequest.end())
+        auto it = _pendingRequests.find(requestId);
+        if (it != _pendingRequests.end())
         {
-            it->second.callback();
-            _pendingRequest.erase(it);
+            it->second.callback(Response{});
+            _pendingRequests.erase(it);
         }
     }
 
     void close()
     {
-        _pendingRequest.clear();
+        _pendingRequests.clear();
         _closed = true;
     }
 };
@@ -60,17 +87,6 @@ int main(int argc, char** argv) {
 
     Client client(channel);
 
-    client.invoke(1, [] { std::cout << "Request 1" << std::endl; });
-    client.invoke(2, [] { std::cout << "Request 2" << std::endl; });
-    client.invoke(3, [] { std::cout << "Request 3" << std::endl; });
-    client.invoke(4, [] { std::cout << "Request 4" << std::endl; });
-    client.invoke(5, [&] {
-        done = true;
-        client.close();
-        std::cout << "Closed" << std::endl;
-    });
-
-
     std::thread server([channel,&client,&done] {
        while (!done) {
            if (channel->size() > 0) {
@@ -81,7 +97,29 @@ int main(int argc, char** argv) {
        }
     });
 
+    std::array<std::thread,5> threads;
+    for (int i=0; i<threads.size(); i++) {
+        threads[i] = std::thread([&]{
+            std::cout << "Invoking request" << i << std::endl;
+            client.invoke(i, [&] (Response resp) { std::cout << "Request:" << i << " done" << std::endl; });
+        });
+    }
+
+    std::cout << "Waiting for RPC..." << std::endl;
+    for (auto& t : threads) {
+        t.join();
+    }
+
+    std::cout << "Last RPC" << std::endl;
+    client.invoke(100, [&] (Response resp) {
+        done = true;
+        client.close();
+        std::cout << "Client closed" << std::endl;
+    });
+
     server.join();
+
+    std::cout << "Done" << std::endl;
 
     return 0;
 }
